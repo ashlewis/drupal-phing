c32f538d2b4ceadcc5a8df7d47b4f7a113112ae3
ash@s8080.com <ash@s8080.com>
2015-02-05 13:45:24 +0000

    - create files directory (if required) - add fix permissions task

--- drush/aliases.drushrc.php
+++ drush/aliases.drushrc.php
@@ -1,18 +1,18 @@
 <?php
 
 $aliases["${env.name}"] = array (
-  'root' => '${project.root}/${drupal.root}/',
+  'root' => '${project.root}/${drupal.dir.root}/',
   'uri' => 'http://${project.url}',
   '#name' => '${env.name}',
   'path-aliases' => array(
-    '%dump-dir' => '${project.root}/private/drush_dump',
-    '%files' => 'sites/default/files',
-    '%private-files' => '${project.root}/private/uploads'
+    '%dump-dir' => '${project.root}/${drupal.dir.private}/drush_dump',
+    '%files' => '${drupal.dir.files}',
+    '%private-files' => '${project.root}/${drupal.dir.private}/uploads'
   )
 );
 
 $aliases["dev"] = array (
-  'root' => '${project.root.dev}/${drupal.root}/',
+  'root' => '${project.root.dev}/${drupal.dir.root}/',
   'uri' => 'http://${project.url.dev}',
   '#name' => 'dev',
   // FQDN or IP of server hosting site (think ssh user@remote-host) 
@@ -20,14 +20,14 @@ $aliases["dev"] = array (
   // A user on the remote host for which you have an ssh key set up 
   'remote-user' => '${os.user.dev}',
   'path-aliases' => array(
-    '%dump-dir' => '${project.root.dev}/private/drush_dump',
-    '%files' => 'sites/default/files',
-    '%private-files' => '${project.root.dev}/private/uploads',
+    '%dump-dir' => '${project.root.dev}/${drupal.dir.private}/drush_dump',
+    '%files' => '${drupal.dir.files}',
+    '%private-files' => '${project.root.dev}/${drupal.dir.private}/uploads',
   )
 );
 
 $aliases["staging"] = array (
-  'root' => '${project.root.staging}/${drupal.root}/',
+  'root' => '${project.root.staging}/${drupal.dir.root}/',
   'uri' => 'http://${project.url.staging}',
   '#name' => 'staging',
   // FQDN or IP of server hosting site (think ssh user@remote-host) 
@@ -35,8 +35,8 @@ $aliases["staging"] = array (
   // A user on the remote host for which you have an ssh key set up 
   'remote-user' => '${os.user.staging}',
   'path-aliases' => array(
-    '%dump-dir' => '${project.root.staging}/private/drush_dump',
-    '%files' => 'sites/default/files',
-    '%private-files' => '${project.root.staging}/private/uploads',
+    '%dump-dir' => '${project.root.staging}/${drupal.dir.private}/drush_dump',
+    '%files' => '${drupal.dir.files}',
+    '%private-files' => '${project.root.staging}/${drupal.dir.private}/uploads',
   )
 );
